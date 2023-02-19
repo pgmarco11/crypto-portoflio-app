44af6b60756ba818fa3e3d211b6c744c60a811f3
Peter G <petergmarco@gmail.com>
2023-02-19 00:46:54 -0500

    added sorting for analysis data

--- src/components/CoinAnalysis.js
+++ src/components/CoinAnalysis.js
@@ -1,52 +1,81 @@
-import {useState, useEffect, useId} from "react"
+import {useState, useEffect} from "react"
 import {useNavigate, useParams, Routes, Route } from "react-router-dom"
 import api from '../api/portfolios';
 import axios from 'axios';
 
 const CoinAnalysis = (props) => {
 
-    const [coinData, setCoinData] = useState([]);    
+    const [coinData, setCoinData] = useState([]);     
+    const [marketCap, setMarketCap] = useState(null);
+    const [volume, setVolume] = useState(null);
 
-    console.log("PortfolioCoin props coinId: "+props.coinId);
+    console.log("PortfolioCoin props coinId: "+props.id);
 
-
-      useEffect(() => {
+    useEffect(() => {
         
-        axios.get(`https://api.coingecko.com/api/v3/coins/${props.coinId}`)
+        axios.get(`https://api.coingecko.com/api/v3/coins/${props.id}`)
           .then(res => {
-                setCoinData(res.data); 
+               console.log("res.data: "+res.data); 
+               setCoinData(res.data); 
             })
             .catch(err => {
-                console.error(err);
-                
+                console.error(err);                
             });
-    }, [props.coinId]);
+    }, [id]);
 
-  
-  
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${props.id}`);
+            console.log("response.data.market_cap: "+response.data.market_data.market_cap.usd);             
+
+            // const formattedMarketCap = coinData.market_data ? new Intl.NumberFormat('en-US', {
+            //     style: 'currency',
+            //     currency: 'USD'
+            //   }).format(coinData.market_data.market_cap.usd) : "";
+
+              setMarketCap(response.data.market_data.market_cap.usd);
+
+        };
+        fetchData();
+    }, [props.id]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${props.id}`);
+            console.log("response.data.total_volume: "+response.data.market_data.total_volume.usd);             
+
+            // const formattedVolume = coinData.market_data ? new Intl.NumberFormat('en-US', {
+            //     style: 'currency',
+            //     currency: 'USD'
+            //   }).format(coinData.market_data.total_volume.usd) : "";
+
+              setVolume(response.data.market_data.total_volume.usd);
+              
+        };
+        fetchData();
+    }, [props.id]);
+
+    
+    console.log("coinData.market_data: "+coinData.market_data);       
     
 
     return (          
         <div>
             <div className="item rowCell" align="left">
-            {props.coinId} 
+            {coinData.name} 
             </div>
             <div className="item rowCell" align="left">
-            market cap
+            {marketCap}
             </div>
             <div className="item rowCell" align="left">
-            volume
+            {volume}
             </div>
             <div className="item rowCell" align="left">
-            <button 
-                onClick={() => props.removeCoin(props.coinId) }>
+            <button className="ui red basic button"
+                onClick={() => props.removeCoin(props.id) }>
                 Delete</button>
             </div>  
-        </div>
-        
-
-
-   
+        </div> 
     )
 
 }
